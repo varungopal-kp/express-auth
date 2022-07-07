fe6b5350d4d67240c567411bab438eddaea58c0f
Varun <varun@qubicle.co.in>
2022-07-08 01:53:25 +0530

    initial commit

--- app.js
+++ app.js
@@ -27,7 +27,13 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({ secret: "#1234", resave: false, saveUninitialized: false }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(function (req, res, next) {
